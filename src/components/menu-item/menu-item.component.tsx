66c59bdac52442f9255d1acd6db74594f6f1381a
Derek <derekscolnick@gmail.com>
2021-01-01 19:32:15 +1000

    broke up homepage and added styling

---
+++ src/components/menu-item/menu-item.component.tsx
@@ -0,0 +1,27 @@
+import './menu-item.styles.scss'
+
+interface IMenuItem {
+    title: string,
+    id: number,
+    imageUrl: string,
+    size?: string,
+}
+
+const MenuItem = ({ title, id, imageUrl, size }: IMenuItem) => {
+    return (
+        <div className={`${size} menu-item`}>
+            <div 
+                className='background-image' 
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
